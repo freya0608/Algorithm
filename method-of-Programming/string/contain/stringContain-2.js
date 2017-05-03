3004636e97750af24ea0948dfc097f428e555a5a
freya0608 <freya001@yeah.net>
2017-05-03 22:59:02 +0800

    string contain

---
+++ method-of-Programming/string/contain/stringContain-2.js
@@ -0,0 +1,21 @@
+/**
+ * Created by freya on 2017/5/3.
+ */
+// function stringContain(a, b) {
+//     var hash = 0;
+//     for(var i=0;i<a.length;i++){
+//         hash |= (1<<(a[i]-'A'));
+//     }
+//     for(var j=0;j<b.length;j++){
+//         if((hash&(1<<(b[j]-'A'))) == 0){
+//             return false;
+//         }else {
+//             return true
+//         }
+//     }
+//
+//
+// }
+// var a ='hahhah';
+// var b ='hahhahhhhks';
+// stringContain(a,b);
