ae74141befc41f52caeecac347388ca512cd70dd
freya0608 <freya001@yeah.net>
2017-05-03 23:30:25 +0800

    isPalindrome

---
+++ method-of-Programming/string/isPlindrome/isPalindrome.js
@@ -0,0 +1,21 @@
+/**
+ * Created by freya on 2017/5/3.
+ */
+function isPalindrome(s) {
+    if(s==null||s.length<1){
+        return false;
+    }
+    var front = 0;
+    var back = s.length -1;
+
+    while (front<back){
+        if(s[front] != s[back]){
+            return false;
+        }
+        ++front;
+        --back;
+    }
+    return true;
+}
+s = 'shhs';
+isPalindrome(s);
